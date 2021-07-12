e0b78583e01d26bbcb0be8742e064cd4cac3f96f
Daniel Yang <dy2yang@uwaterloo.ca>
2021-07-12 00:59:08 -0400

    Updates

---
+++ precache-manifest.e94596f25a655a2e0781e630a535de84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00be083d0cec0c9c8cb17524089b931",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06e3b663344b8214a904",
+    "url": "./static/css/main.745c729c.chunk.css"
+  },
+  {
+    "revision": "85c6a423bd6e1f3270bb",
+    "url": "./static/js/2.1c2db1a8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.1c2db1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e3b663344b8214a904",
+    "url": "./static/js/main.1d1afdbd.chunk.js"
+  },
+  {
+    "revision": "bcd75c5352a741029177",
+    "url": "./static/js/runtime-main.ac2f6e42.js"
+  },
+  {
+    "revision": "eb4b4efe1d5b53716597e7e43c10efa2",
+    "url": "./static/media/DOM.eb4b4efe.png"
+  },
+  {
+    "revision": "624f58d34c0c2aa255ce71da3633611f",
+    "url": "./static/media/fadeDistance.624f58d3.png"
+  },
+  {
+    "revision": "26c67cf5320ba0561bd25f5fda5b31b3",
+    "url": "./static/media/noice.26c67cf5.gif"
+  },
+  {
+    "revision": "e7e5e4d7a1ce5cfcc88063acfee0b55f",
+    "url": "./static/media/twitchcover.e7e5e4d7.jpg"
+  }
+]);
