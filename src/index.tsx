e3f659060b8b2b853d6104322de17496324283db
Saiph Games <saiphgames@yandex.com>
2023-11-26 14:12:02 -0400

    implemented subscribable data logic potential undefined data error with old render slides

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import StackedCarousel from './carousel';
 import { props, slideProp } from './interfaces'
+import { PromisedData } from './promiseddata';
 import ResponsiveContainer, {ResponsiveContainerProps} from './responsiveContainer';
 export {
   StackedCarousel,
   ResponsiveContainer,
   ResponsiveContainerProps,
   props as StackedCarouselProps,
-  slideProp as StackedCarouselSlideProps
+  slideProp as StackedCarouselSlideProps,
+  PromisedData
 };
